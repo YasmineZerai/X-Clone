f1cef7d6798fdbd907c7931c009060093f9ae31b
YasmineZerai <zeraiyasmine7@gmail.com>
2024-10-17 19:39:07 +0100

    died inside because all the changes i did are gone im left with whatever this is

--- src/sidebar/sideBar.tsx
+++ src/sidebar/sideBar.tsx
@@ -1,13 +1,13 @@
-import HomeIcon from "../icons/homeIcon";
-import MessageIcon from "../icons/messageIcon";
-import NotificationIcon from "../icons/notificationIcon";
-import PostIcon from "../icons/postIcon";
-import ProfileIcon from "../icons/profileIcon";
-import SearchIcon from "../icons/searchIcon";
-import XIcon from "../icons/xIcon";
+import HomeIcon from "../icons/sideBarIcons/homeIcon";
+import MessageIcon from "../icons/sideBarIcons/messageIcon";
+import NotificationIcon from "../icons/sideBarIcons/notificationIcon";
+import PostIcon from "../icons/sideBarIcons/postIcon";
+import ProfileIcon from "../icons/sideBarIcons/profileIcon";
+import SearchIcon from "../icons/sideBarIcons/searchIcon";
+import XIcon from "../icons/sideBarIcons/xIcon";
 import SideBarComponent from "./sideBarComponent";
 export default function SideBar(){
-    return(<div className="bg-black  h-screen border-r border-stone-900  w-1/6 flex flex-col items-end justify-start pt-2 pr-6">
+    return(<div className="bg-black fixed top-0 left-0 h-screen border-r border-stone-900  w-1/6 flex flex-col items-end justify-start pt-2 pr-6">
         <SideBarComponent>
             <XIcon/>
         </SideBarComponent>
