4dbd16c7f543c9670cb36453ae025abda7ccf865
YasmineZerai <zeraiyasmine7@gmail.com>
2024-10-19 13:45:35 +0100

    created a show more button

--- src/Timeline/timeline.tsx
+++ src/Timeline/timeline.tsx
@@ -1,9 +1,11 @@
 import Post from "./post";
+import ShowMoreButton from "./ShowMoreButton";
 import TimelineHeader from "./timelineHeader";
 
 export default function Timeline(){
     return (<div className="bg-black border-r ml-[16.4%] h-screen w-3/6 border-stone-900 flex flex-col align-top">
         <TimelineHeader/>
         <Post/>
+        <ShowMoreButton number={20}/>
     </div>)
 }
