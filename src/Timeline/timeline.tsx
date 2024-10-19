4e5b5047902d06a8ff8c541561fa0a43c40ba825
YasmineZerai <zeraiyasmine7@gmail.com>
2024-10-19 13:43:28 +0100

    created post

--- src/Timeline/timeline.tsx
+++ src/Timeline/timeline.tsx
@@ -1,7 +1,9 @@
+import Post from "./post";
 import TimelineHeader from "./timelineHeader";
 
 export default function Timeline(){
-    return (<div className="bg-black border-r h-screen w-3/6 border-stone-900 flex flex-col align-top">
+    return (<div className="bg-black border-r ml-[16.4%] h-screen w-3/6 border-stone-900 flex flex-col align-top">
         <TimelineHeader/>
+        <Post/>
     </div>)
 }
